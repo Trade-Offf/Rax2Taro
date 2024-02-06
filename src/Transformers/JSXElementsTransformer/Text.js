d95c35d3edd595861d89ab98a8cf6d1501489943
玄器 <ljl353638@alibaba-inc.com>
2024-02-06 17:54:37 +0800

    update: Text transformer

--- src/Transformers/JSXElementsTransformer/Text.js
+++ src/Transformers/JSXElementsTransformer/Text.js
@@ -1,6 +1,4 @@
-const { traverse, t } = require("../../../utils");
-
 function transformTextElement(path) {
-  // 针对 Text 组件的转换逻辑
+  // Taro 与 Rax 的 Text 组件属性一致，无需额外处理
 }
 module.exports = { transformTextElement };
